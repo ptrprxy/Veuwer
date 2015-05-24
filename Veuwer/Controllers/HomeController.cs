41f61e57c4afabb1ae0563c4a5a3b478dfffbb8d
Icy Defiance <icydefiance@gmail.com>
2015-05-24 13:59:02 -0400

    Ajax upload, multi-upload, galleries

--- Veuwer/Controllers/HomeController.cs
+++ Veuwer/Controllers/HomeController.cs
@@ -23,42 +23,50 @@ namespace Veuw.Controllers
         [HttpPost]
         public ActionResult Upload()
         {
-            object errorjson = null;
+            List<ImageLink> newLinks = new List<ImageLink>();
+            for (int i = 0; i < Request.Files.Count; i++)
+			{
+                var image = Request.Files[i];
+                if (image.ContentLength < 0 || image.ContentLength > 2097152) //2 mb limit
+                    return Fail(image.FileName + " too large");
 
-            var image = Request.Files[0];
-            if (image.ContentLength > 0 && image.ContentLength < 2097152) //2 mb limit
-            {
                 var hash = BitConverter.ToString(sha.ComputeHash(image.InputStream)).Replace("-", String.Empty);
                 var imgMatch = db.Images.FirstOrDefault(x => x.Hash == hash) ?? new Image() { Hash = hash };
                 var imgLink = new ImageLink() { Image = imgMatch };
 
                 var img = System.Drawing.Image.FromStream(image.InputStream);
                 image.InputStream.Seek(0, SeekOrigin.Begin);
-                if (ImageFormat.Jpeg.Equals(img.RawFormat) || ImageFormat.Png.Equals(img.RawFormat) || ImageFormat.Gif.Equals(img.RawFormat))
+                if (!ImageFormat.Jpeg.Equals(img.RawFormat) && !ImageFormat.Png.Equals(img.RawFormat) && !ImageFormat.Gif.Equals(img.RawFormat))
+                    return Fail(image.FileName + " is not a supported file");
+
+                db.ImageLinks.Add(imgLink);
+                newLinks.Add(imgLink);
+
+                if (imgMatch.ImgBlob == null)
                 {
-                    db.ImageLinks.Add(imgLink);
-                    db.SaveChanges();
-
-                    if (imgMatch.ImgBlob == null)
-                    {
-                        imgMatch.ImgBlob = new byte[image.ContentLength];
-                        image.InputStream.Read(imgMatch.ImgBlob, 0, image.ContentLength);
-                        imgMatch.MimeType = image.ContentType;
-                        db.SaveChanges();
-                    }
-
-                    Response.Redirect("/" + Encode(imgLink.Id));
-                    return Json(new { status = "success" }, JsonRequestBehavior.AllowGet);
+                    imgMatch.ImgBlob = new byte[image.ContentLength];
+                    image.InputStream.Read(imgMatch.ImgBlob, 0, image.ContentLength);
+                    imgMatch.MimeType = image.ContentType;
                 }
             }
 
-            return Json(new { status = "failure" }, JsonRequestBehavior.AllowGet);
+            db.SaveChanges();
+            return Json(new { status = "success", message = "/" + Encode(newLinks[0].Id) + "?g=" + string.Join(",", newLinks.Skip(1).Select(x => Encode(x.Id))) }, JsonRequestBehavior.AllowGet);
+        }
+
+        JsonResult Fail(string reason)
+        {
+            return Json(new { status = "failure", message = reason }, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Images(string id)
         {
-            ViewBag.ImageUri = id;
-            return View();
+            var ids = new[] { id }.AsEnumerable();
+            var galleryIds = Request.QueryString.GetValues("g");
+            if (galleryIds != null)
+                ids = ids.Concat(galleryIds[0].Split(','));
+
+            return View(ids);
         }
 
         public ActionResult ImageDirect(string id)
--- Veuwer/Scripts/Main/index.js
+++ Veuwer/Scripts/Main/index.js
@@ -1,12 +1,60 @@
 ﻿$(function () {
     $('#filepicker').change(function () {
-        $('#filelist').html($.map(this.files, function (x) {
+        var startEntry = '<div class="row fileentry"><div class="col-md-10">';
+        var endEntry = '</div></div>';
+
+        $('#filelist').html(startEntry + $.map(this.files, function (x) {
             return x.name;
-        }).join("<br>"));
+        }).join(endEntry + startEntry) + endEntry);
+
         if (this.files.length > 0) {
             $('#fileupload').removeAttr('disabled');
         } else {
             $('#fileupload').attr("disabled", true);
         }
     });
+
+    $('#fileupload').click(function () {
+        $('#uploaderror').css({ display: 'none' });
+        $('#uploadprog').css({ display: 'block' });
+        var formData = new FormData($('form')[0]);
+        $.ajax({
+            url: '/Home/Upload',
+            type: 'POST',
+            xhr: function () {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    myXhr.upload.addEventListener('progress', fileprog, false);
+                }
+                return myXhr;
+            },
+            //beforeSend: beforeSendHandler,
+            success: filesuccess,
+            error: fileerror,
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            dataType: 'json'
+        });
+    });
 });
+
+function fileprog(e) {
+    if (e.lengthComputable) {
+        $('#uploadprog .progress-bar').attr({ 'aria-valuenow': e.loaded, 'aria-valuemax': e.total });
+        var compPer = (e.loaded / e.total * 100).toString() + '%';
+        $('#uploadprog .progress-bar').css({ width: compPer });
+        $('#uploadprog .progress-bar .sr-only').html(compPer + ' Complete');
+    }
+}
+
+function filesuccess(e) {
+    $('#uploadprog .progress-bar').addClass('progress-bar-success');
+    window.location.href = e.message;
+}
+
+function fileerror(e) {
+    $('#uploadprog').css({ display: 'none' });
+    $('#uploaderror').css({ display: 'block' }).html(e.message);
+}
