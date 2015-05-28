abb11c9bfe5fb70c7f48d9d9f0df8935f7e221bd
Icy Defiance <icydefiance@gmail.com>
2015-05-27 21:31:18 -0400

    Upload from url, individual progress bars

--- Veuwer/Scripts/Main/index.js
+++ Veuwer/Scripts/Main/index.js
@@ -1,67 +1,118 @@
-﻿$(function () {
-    $('#filepicker').change(function () {
-        var startEntry = '<div class="row fileentry"><div class="col-md-10">';
-        var endEntry = '</div></div>';
+﻿var toupload;
+var forward = '';
 
-        $('#filelist').html(startEntry + $.map(this.files, function (x) {
-            return x.name;
-        }).join(endEntry + startEntry) + endEntry);
+$(function () {
+    toupload = [];
 
-        if (this.files.length > 0) {
-            $('#fileupload').removeAttr('disabled');
-        } else {
-            $('#fileupload').attr("disabled", true);
+    $('#filestreams').change(function () {
+        for (var i = 0; i < this.files.length; i++) {
+            toupload.push(this.files[i]);
         }
+        console.log(toupload);
+        refreshFileList();
     });
 
     $('#fileupload').click(function () {
-        $('#uploaderror').css({ display: 'none' });
-        $('#uploadprog').css({ display: 'block' });
-        $('#fileupload,#filepicker-label').attr("disabled", true);
-        var formData = new FormData($('form')[0]);
-        $.ajax({
-            url: '/Home/Upload',
-            type: 'POST',
-            xhr: function () {
-                var myXhr = $.ajaxSettings.xhr();
-                if (myXhr.upload) {
-                    myXhr.upload.addEventListener('progress', fileprog, false);
-                }
-                return myXhr;
-            },
-            //beforeSend: beforeSendHandler,
-            success: filesuccess,
-            error: fileerror,
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            dataType: 'json'
+        $('uploaderror').css({ display: 'none' });
+        $('.progress').css({ display: 'block' });
+        $('#filestreams,#filestreams-label,#urlinput').attr("disabled", true);
+        $('#uploaderror').html('');
+
+        var filedivs = $('.fileentry');
+        var ajaxlist = [];
+        for (var i = 0; i < toupload.length; i++) {
+            var formdata = new FormData();
+            formdata.append('image', toupload[i]);
+            console.log(toupload[i]);
+            console.log(formdata);
+
+            var index = i;
+            ajaxlist.push($.ajax({
+                url: '/Home/Upload',
+                type: 'POST',
+                xhr: createProg(i),
+                success: createSuccess(i),
+                error: createError(i),
+                data: formdata,
+                cache: false,
+                contentType: false,
+                processData: false,
+                dataType: 'json'
+            }));
+        }
+
+        $.when.apply($, ajaxlist).then(function () {
+            window.location.href = forward.substring(1);
         });
+    });
+
+    $('#urlinput').on('paste', function () {
+        setTimeout(function () {
+            var urlinput = $('#urlinput')[0].value;
+            toupload.push(urlinput);
+            urlinput.value = '';
+            refreshFileList();
+        }, 100);
     });
 });
 
-function fileprog(e) {
-    if (e.lengthComputable) {
-        $('#uploadprog .progress-bar').attr({ 'aria-valuenow': e.loaded, 'aria-valuemax': e.total });
-        var compPer = (e.loaded / e.total * 100).toString() + '%';
-        $('#uploadprog .progress-bar').css({ width: compPer });
-        $('#uploadprog .progress-bar .sr-only').html(compPer + ' Complete');
+function createProg(i) {
+    return function () {
+        var myXhr = $.ajaxSettings.xhr();
+        if (myXhr.upload) {
+            myXhr.upload.addEventListener('progress', function (e) {
+                if (e.lengthComputable) {
+                    $($('.progress .progress-bar')[i]).attr({ 'aria-valuenow': e.loaded, 'aria-valuemax': e.total });
+                    var compPer = (e.loaded / e.total * 100).toString() + '%';
+                    $($('.progress .progress-bar')[i]).css({ width: compPer });
+                    $($('.progress .progress-bar .sr-only')[i]).html(compPer + ' Complete');
+                }
+            }, false);
+        }
+        return myXhr;
+    };
+}
+
+function createSuccess(i) {
+    return function (e) {
+        $($('.progress .progress-bar')[i]).addClass('progress-bar-success');
+        forward += ',' + e.newid;
+    };
+}
+
+function createError(i) {
+    return function (e) {
+        $($('.progress .progress-bar')[i]).addClass('progress-bar-danger');
+        $('#uploaderror').css({ display: 'block' });
+        var errordiv = $('#uploaderror');
+        if ('message' in e) {
+            errordiv.html(errordiv.html() + '<br>' + e.message);
+        } else {
+            errordiv.html(errordiv.html() + '<br>' + 'There was an error while uploading.');
+        }
+        $('#filestreams,#filestreams-label,#urlinput').removeAttr('disabled');
     }
 }
 
-function filesuccess(e) {
-    $('#uploadprog .progress-bar').addClass('progress-bar-success');
-    window.location.href = e.message;
-}
+function refreshFileList() {
+    var startEntry = '<div class="row fileentry"><div class="col-md-10">';
+    var endEntry = '<div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%"><span class="sr-only">0% Complete</span></div></div></div></div>';
+    var filestreams = $('#filestreams')[0];
 
-function fileerror(e) {
-    $('#uploadprog').css({ display: 'none' });
-    $('#uploaderror').css({ display: 'block' });
-    if ('message' in e) {
-        $('#uploaderror').html(e.message);
+    $('#filelist').html(startEntry + $.map(toupload, function (x) {
+        if (typeof x === 'string') {
+            var url = x.replace(/^.*[\\\/]/, '');
+            if (url === '')
+                url = x;
+            return url;
+        } else {
+            return x.name;
+        }
+    }).join(endEntry + startEntry) + endEntry);
+
+    if (toupload.length > 0) {
+        $('#fileupload').removeAttr('disabled');
     } else {
-        $('#uploaderror').html('There was an error while uploading.');
+        $('#fileupload').attr("disabled", true);
     }
-    $('#fileupload,#filepicker-label').removeAttr('disabled');
 }
