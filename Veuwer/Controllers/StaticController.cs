ee622e2ff70f7ec7244ef756afdf0a7149ea1ef2
Icy Defiance <icydefiance@gmail.com>
2015-06-06 13:51:10 -0400

    Add transparency report

---
+++ Veuwer/Controllers/StaticController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Veuwer.Controllers
+{
+    public class StaticController : Controller
+    {
+        public ActionResult Transparency()
+        {
+            return View();
+        }
+    }
+}
